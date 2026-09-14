b75bb08017fc6f18332d1e33cb37b7f9d5efc1c6
emilkc-code <emilkellerchristensen@gmail.com>
2026-09-14 16:57:23 +0200

    Added acceloration to paddle

--- project/Paddle.java
+++ project/Paddle.java
@@ -1,73 +1,50 @@
 import greenfoot.*;
 
-
-/**
- * A paddle is an object that goes back and forth. Though it would be nice if balls would bounce of it.
- * 
- * @author The teachers 
- * @version 1
- */
 public class Paddle extends Actor
 {
+    private final float DRAG_COEFFICIENT = 0.9f;
+    private final float ACCELORATION_FORCE = 0.1f;
+    
     private int width;
     private int height;
-    private int dx;
-    private int speed;
+    private float positionX = 0;
+    private int maxSpeed;
+    private float velocity;
+    private int acceloration = 0;
 
-    /**
-     * Constructs a new paddle with the given dimensions.
-     */
-    public Paddle(int width, int height, int sped)
-    {
+    public Paddle(int width, int height, int maxSpeed) {
         this.width = width;
         this.height = height;
-        this.speed = sped;
-        dx = 1;
+        //positionX = getX();
+        this.maxSpeed = maxSpeed;
         createImage();
-        
-    }
-
-    /**
-     * Act - do whatever the Paddle wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        //tryChangeDirection();
-        inputChecker();
-        //setLocation(getX() + dx, getY());
-    }    
-
-    /**
-     * Will rotate the paddle 180 degrees if the paddle is at worlds edge.
-     */
-    private void tryChangeDirection()
-    {
-        //Check to see if we are touching the outer boundaries of the world:
-        // IF we are touching the right boundary OR we are touching the left boundary:
-        if(getX() + width/2 >= getWorld().getWidth() || getX() - width/2 <= 0)
-        {
-            //Change our 'x' direction to the inverted direction:
-            dx = dx * -1;
-        }
-    }
-
-    private void inputChecker()
-    {
-        if ((Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")) && this.getX() > width / 2) {
-            this.setLocation(getX() - speed, getY());
-        }
-        if ((Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")) && this.getX() < getWorld().getWidth() - width / 2) {
-            this.setLocation(getX() + speed, getY());
-        }
-        
     }
     
-    /**
-     * Creates and sets an image for the paddle, the image will have the same dimensions as the paddles width and height.
-     */
-    private void createImage()
-    {
+    public void act() {
+        inputChecker();
+        applyMovement();
+    }
+
+    private void inputChecker() {
+        if (!Greenfoot.isKeyDown("a") && !Greenfoot.isKeyDown("left") && !Greenfoot.isKeyDown("d") && !Greenfoot.isKeyDown("right")) {
+            acceloration = 0;
+            velocity *= DRAG_COEFFICIENT;
+        }
+        
+        if ((Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left"))) { acceloration = -1; }
+        if ((Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right"))) { acceloration = 1; }
+    }
+    
+    private void applyMovement() {
+        velocity += acceloration * ACCELORATION_FORCE;
+        if (this.getX() > getWorld().getWidth() - width / 2) { velocity = Math.clamp(velocity, -maxSpeed, 0); }
+        if (this.getX() < width / 2) { velocity = Math.clamp(velocity, 0, maxSpeed); }
+        velocity = Math.clamp(velocity, -maxSpeed, maxSpeed);
+        positionX += velocity;
+        setLocation((int) positionX, getY());
+    }
+    
+    private void createImage() {
         GreenfootImage img = new GreenfootImage("ct.png");
         img.scale(this.width, this.height);
         setImage(img);
--- project/PingWorld.java
+++ project/PingWorld.java
@@ -1,41 +1,23 @@
 import greenfoot.*;
 
-
-/**
- * The Ping World is where Balls and Paddles meet to play pong.
- * 
- * @author The teachers 
- * @version 1
- */
 public class PingWorld extends World
 {
     private static final int WORLD_WIDTH = 500;
     private static final int WORLD_HEIGHT = 700;
     
-    private Paddle botPaddle = new Paddle(100,20, 2);
-    private AIPaddle topPaddle = new AIPaddle(100, 20, 1);
-    private Ball ball = new Ball(botPaddle, topPaddle);
-
-    /**
-     * Constructor for objects of class PingWorld.
-     */
-    public PingWorld(boolean gameStarted)
-    {
-        super(WORLD_WIDTH, WORLD_HEIGHT, 1); 
-        if (gameStarted)
-        {
+    public PingWorld(boolean gameStarted) {
+        super(WORLD_WIDTH, WORLD_HEIGHT, 1);
+        
+        if (gameStarted) {
             levelText(1);
             
-            // Create a new world with WORLD_WIDTHxWORLD_HEIGHT cells with a cell size of 1x1 pixels.
-            addObject(new Background(WORLD_WIDTH, WORLD_HEIGHT),WORLD_WIDTH/2, WORLD_HEIGHT/2);
-            addObject(botPaddle, 60, WORLD_HEIGHT - 50);
-            addObject(ball, WORLD_WIDTH/2, WORLD_HEIGHT/2);
-            addObject(topPaddle, 60, WORLD_HEIGHT - 650);
+            addObject(new Background(getHeight(), getHeight()),getHeight()/2, getHeight()/2);
+            addObject(new AIPaddle(100, 20, 1), 60, getHeight() - 650);
+            addObject(new Paddle(100,20, 2), 60, getHeight() - 50);
+            addObject(new Ball(getObjects(Paddle.class).get(0), getObjects(AIPaddle.class).get(0)), getHeight()/2, getHeight()/2);
         }
-        else
-        {
-            Greenfoot.setWorld(new IntroWorld());
-        }
+        
+        else { Greenfoot.setWorld(new IntroWorld()); }
     }
     
     public void levelText(int n) { showText("Speed:" + Integer.toString(n), 40, 16); }
