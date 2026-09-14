18f09948c0c5b73db0bb3c52b640ec5e1737ec33
emilkc-code <emilkellerchristensen@gmail.com>
2026-09-14 15:19:42 +0200

    Fixed broken code

--- project/Ball.java
+++ project/Ball.java
@@ -7,10 +7,10 @@ public class Ball extends Actor
     private final int STARTING_ANGLE_WIDTH = 90;
     private final int DELAY_TIME = 100;
     private final int HITS_FOR_SPEED = 4;
+    private final int MAX_ANGLE = 60;
 
     private int speed;
     private int ownHits;
-    private boolean hasBouncedHorizontally;
     private int delay;
     
     private Paddle bottomPaddle;
@@ -43,9 +43,7 @@ public class Ball extends Actor
         }
     }    
 
-    private boolean isTouchingSides() {
-        return (getX() <= BALL_SIZE/2 || getX() >= getWorld().getWidth() - BALL_SIZE/2);
-    }
+    private boolean isTouchingSides() { return (getX() <= BALL_SIZE/2 || getX() >= getWorld().getWidth() - BALL_SIZE/2); }
     
     private boolean isTouchingCeiling() { return (getY() <= BALL_SIZE/2); }
     private boolean isTouchingFloor() { return (getY() >= getWorld().getHeight() - BALL_SIZE/2); }
@@ -53,8 +51,8 @@ public class Ball extends Actor
     private void bounceWalls() {
         if (!isTouchingSides()) { return; }
         
-        if (90 < getRotation() && getRotation() < 270) { setRotation(180 - getRotation()); }
-        if (0 < getRotation() && getRotation() < 90 || 270 < getRotation() && getRotation() < 360) { setRotation(180 - getRotation()); }
+        if (90 < getRotation() && getRotation() < 270 && getX() < getWorld().getWidth() / 2) { setRotation(180 - getRotation()); }
+        if ((0 < getRotation() && getRotation() < 90 || 270 < getRotation() && getRotation() < 360) && getX() > getWorld().getWidth() / 2) { setRotation(180 - getRotation()); }
         
         GreenfootSound onPaddleHit = new GreenfootSound("WallThudSound.mp3");
         onPaddleHit.play();
@@ -76,13 +74,23 @@ public class Ball extends Actor
         pingWorld.moneyText(gameManager.getMoney());
     }
     
+    private void checkBounceOffPaddleTop() {
+        if (getIntersectingObjects(AIPaddle.class).size() < 1 || getRotation() < 180) { return; }
+        
+        AIPaddle paddle = (AIPaddle) getIntersectingObjects(AIPaddle.class).get(0);
+        turnAwayFrom(paddle.getX(), paddle.getY(), true);
+        
+        GreenfootSound bombBeepSound = new GreenfootSound("BombBeep.mp3");
+        bombBeepSound.play();
+    }
+    
     private void checkBounceOffPaddleBottom() {
-        if (getIntersectingObjects(Paddle.class).size() < 1 || getRotation() < 180) { return; }
+        if (getIntersectingObjects(Paddle.class).size() < 1 || getRotation() > 180) { return; }
         ownHits += 1;
         hitPaddle();
     
         Paddle paddle = (Paddle) getIntersectingObjects(Paddle.class).get(0);
-        turnAwayFrom(paddle.getX(), paddle.getY());
+        turnAwayFrom(paddle.getX(), paddle.getY(), false);
     }
     
     private void hitPaddle() {
@@ -99,25 +107,15 @@ public class Ball extends Actor
         }
     }
     
-    private void checkBounceOffPaddleTop() {
-        if (getIntersectingObjects(AIPaddle.class).size() < 1 || getRotation() > 180) { return; }
-        
-        AIPaddle paddle = (AIPaddle) getIntersectingObjects(AIPaddle.class).get(0);
-        turnAwayFrom(paddle.getX(), paddle.getY());
-        
-        GreenfootSound bombBeepSound = new GreenfootSound("BombBeep.mp3");
-        bombBeepSound.play();
-    }
-    
-    private void turnAwayFrom(int x, int y) {
+    private void turnAwayFrom(int x, int y, boolean goingUp) {
         turnTowards(x, y);
-        setRotation(180 + getRotation());
+        if (goingUp) { setRotation(Math.clamp((180 + getRotation()) % 360, 90 - MAX_ANGLE, 90 + MAX_ANGLE)); }
+        else { setRotation(Math.clamp((180 + getRotation()) % 360, 270 - MAX_ANGLE, 270 + MAX_ANGLE)); }
     }
     
     private void init(boolean reset) {
         speed = 2;
         delay = DELAY_TIME;
-        hasBouncedHorizontally = false;
         ownHits = 0;
         
         if (pingWorld != null) { pingWorld.levelText(1); }
