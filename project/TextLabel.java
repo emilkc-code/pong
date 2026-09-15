c08ba177051191c4e2dadae0c299f17d89f7237d
emilkc-code <emilkellerchristensen@gmail.com>
2026-09-15 11:05:16 +0200

    forgot text label

---
+++ project/TextLabel.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+public class TextLabel extends Actor {
+    public TextLabel(String text, int fontSize) {
+        updateText(text, fontSize);
+    }
+    
+    public void updateText(String text, int fontSize) {
+        int width = text.length() * (fontSize / 2) * 2;
+        int height = fontSize + 10;
+        
+        GreenfootImage img = new GreenfootImage(width, height);
+        Font font = new Font(true, false, fontSize);
+        img.setFont(font);
+        img.setColor(Color.WHITE);
+        
+        img.drawString(text, width / 4, fontSize);
+        
+        setImage(img);
+    }
+}
